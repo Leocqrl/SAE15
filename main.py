3e8751ba43e43b6a2452b59fd3ddf974b3bc42cb
Leocqrl <2810leo768@gmail.com>
2025-01-06 10:37:31 +0100

    Squelette pour explorer notre fichier csv avec python

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv
+
+# Premièrement, on va ouvrir notre fichier csv
+# On précisera que les lignes sont séparer par '' et les cases sont délimiter par ';'
+with open('experimentations_5G.csv', newline='') as file:
+    read=csv.reader(file, delimiter=';')
+    #On va ensuite parcourir notre feuille csv : ligne par ligne
+    for row in read:
+        ...

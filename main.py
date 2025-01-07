2c9a230a0aae2c60bbe31f317fb0fe71f0fed64a
Amine212-A <amine.elkhalki.lina@gmail.com>
2025-01-07 09:53:54 +0100

    sync

--- main.py
+++ main.py
@@ -7,3 +7,24 @@ with open('experimentations_5G.csv', newline='') as file:
     #On va ensuite parcourir notre feuille csv : ligne par ligne
     for row in read:
         ...
+        
+
+
+# Position [latitude, longitude] sur laquelle est centrée la carte
+location = [47, 1]
+
+# Niveau de zoom initial
+zoom = 6
+
+# Style de la carte
+tiles = 'cartodbpositron'
+
+# Création de la carte
+Carte = folium.Map(location=location,
+                   zoom_start=zoom,
+                   tiles=tiles)
+
+# Sauvegarde dans un fichier HTML
+Carte.save('carte_interactive.html')
+
+print("Carte enregistrée sous 'carte_interactive.html'")

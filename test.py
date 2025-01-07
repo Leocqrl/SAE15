d7689e849bb1029ed4b400329a85186962f4f827
Leocqrl <2810leo768@gmail.com>
2025-01-07 10:42:29 +0100

    ajout de tout les marqueurs en auto via les coordonnées

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-import folium
-a=49.4783333333333
-b=0.126111111111111
+import csv, folium
+
 m=folium.Map(
     max_bounds=True,
     zoom_start=6,
     location=(47, 1),
     )
 fg=folium.FeatureGroup(name="Icon collection", control=False).add_to(m)
-folium.Marker(location=(a,b)).add_to(fg)
+
+# Premièrement, on va ouvrir notre fichier csv
+# On précisera que les lignes sont séparer par '' et les cases sont délimiter par ';'
+with open('experimentations_5G.csv', newline='', encoding='cp1252') as file:
+    read=csv.reader(file, delimiter=';')
+    #On va ensuite parcourir notre feuille csv : ligne par ligne
+    for i, row in enumerate(read):
+        if i!=0:
+            folium.Marker(location=(float(row[6].replace(',','.')),float(row[7].replace(',','.')))).add_to(fg)
 
 m.save("index.html")

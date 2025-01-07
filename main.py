e9da55f166711624e83c2aba948bf6466db1be02
raphcllt <raphaelcollet87@gmail.com>
2025-01-07 11:22:58 +0100

    corretion

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import csv
-from collections import defaultdict
 
 # Création d'un dictionnaire qui va contenir une liste d'entreprises pour chaque région
-regions_dict = defaultdict(list)
-
+regions_dict ={}
 # Ouverture du fichier CSV
 with open('experimentations_5G.csv', newline='', encoding='cp1252') as file:
     read = csv.reader(file, delimiter=';')
     
     # Parcours du fichier CSV ligne par ligne
     for row in read:
-        # Vérifier si la ligne n'est pas vide
-        if row:
-            try:
-                entreprise = row[0]  # Entreprise (colonne 0)
-                region = row[11]     # Région (colonne 11)
-                
-                # Ajouter l'entreprise à la liste correspondant à la région
-                regions_dict[region].append(entreprise)
-            except IndexError:
-                # Si la ligne ne contient pas suffisamment de colonnes, on passe à la suivante
-                continue
+        entreprise = row[0]  # Entreprise (colonne 0)
+        region = row[11]     # Région (colonne 11)
+            
+        # Ajouter l'entreprise à la liste correspondant à la région
+        
+        
+
+
 
 # Affichage du dictionnaire des régions avec les entreprises
-for region, entreprises in regions_dict.items():
-    print(f"Région: {region}")
-    for entreprise in entreprises:
-        print(f"  - {entreprise}")
+#for region, entreprises in regions_dict.items():
+#    print(f"Région: {region}")
+#    for entreprise in entreprises:
+#        print(f"  - {entreprise}")

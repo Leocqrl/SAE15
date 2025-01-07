c5749b52c0aa51a57618e82e5954c7570a91612f
Adjaaaa <adjadbn@gmail.com>
2025-01-07 10:27:58 +0100

    ajout de l'encodage et de la liste

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import csv
-
+L=[]
 # Premièrement, on va ouvrir notre fichier csv
 # On précisera que les lignes sont séparer par '' et les cases sont délimiter par ';'
-with open('experimentations_5G.csv', newline='') as file:
+with open('experimentations_5G.csv', newline='',encoding='cp1252') as file:
     read=csv.reader(file, delimiter=';')
     #On va ensuite parcourir notre feuille csv : ligne par ligne
     for row in read:
         if row == '':
             pass
         else:
-            print(row[0],row[11])
+            L.append([row[0],row[11]])
+
+print(L)

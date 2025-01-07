0e45c8baffc86045ee2a922062f95964b137b7f5
Leocqrl <2810leo768@gmail.com>
2025-01-07 15:25:35 +0100

    Carte Amélioré

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import csv, folium, branca
+from folium.plugins import MarkerCluster
 
 m=folium.Map(
     max_bounds=True,
     zoom_start=6,
     location=(47, 1),
     )
-fg=folium.FeatureGroup(name="Icon collection", control=False).add_to(m)
+Marker_Cluster=MarkerCluster(name="Icon collection", control=False).add_to(m)
 
 # Premièrement, on va ouvrir notre fichier csv
 # On précisera que les lignes sont séparer par '' et les cases sont délimiter par ';'
@@ -18,14 +19,45 @@ with open('experimentations_5G.csv', newline='', encoding='cp1252') as file:
             #On va ensuite mettre en float et remplacer les ',' par des '.' pour ne pas avoir d'erreur
             lat=float(row[6].replace(',','.'))
             lon=float(row[7].replace(',','.'))
-            #On met dans la variable texte les informations que l'on veut lorsque l'on clique sur chaque Marker un a un
+
+            #On met dans la variable html les informations que l'on veut lorsque l'on clique sur chaque Marker un a un
             html=f"""
             <h5>{row[0]}</h5>
-            <FONT size="2px">
+            <FONT size="1px">
             <p>Bande de fréquences : {row[1]}</p>
             </FONT>
             """
+            taille=100
+            compteur=0
+            for y in range(15, 32):
+                if int(row[y])==1:
+                    taille+=20
+                    compteur+=1
+                    if compteur==1:
+                        html+=f"""
+                        <FONT size="1px">
+                        <h6>Recherche et Développement :</h6>
+                        </FONT>
+                        """
+                    html+=f"""
+                    <FONT size="1px">
+                    <p>{Legende[y]}</p>
+                    </FONT>
+                    """  
+                    
+            iframe=branca.element.IFrame(html, width=150, height=taille)
+            Popup=folium.Popup(iframe, max_width=500, max_height=300 )
+            if compteur>=4:
+                ICON=folium.Icon(color='red')
+            elif 4>compteur>=2:
+                ICON=folium.Icon(color='lightred')
+            elif compteur==1:
+                ICON=folium.Icon(color='orange')
+            elif compteur==0:
+                ICON=folium.Icon(color='lightgray')
             #Création du marker avec toute les informations précédentes
-            folium.Marker(location=(lat,lon), popup=folium.Popup(branca.element.IFrame(html, 125, 75), max_width=125)).add_to(fg)
+            folium.Marker(location=(lat,lon), popup=Popup, icon=ICON).add_to(Marker_Cluster)
+        else :
+            Legende=row
 
 m.save("Carte_Interactive.html")

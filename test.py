08a9f8a4ed9d97090177e9fb7a91e761a1348fe8
Leocqrl <2810leo768@gmail.com>
2025-01-07 11:08:13 +0100

    Ajout de commentaires

--- test.py
+++ test.py
@@ -13,10 +13,14 @@ with open('experimentations_5G.csv', newline='', encoding='cp1252') as file:
     read=csv.reader(file, delimiter=';')
     #On va ensuite parcourir notre feuille csv : ligne par ligne
     for i, row in enumerate(read):
+        #On va retirer la première ligne afin de ne pas être déranger par le nom des colonnes
         if i!=0:
+            #On va ensuite mettre en float et remplacer les ',' par des '.' pour ne pas avoir d'erreur
             lat=float(row[6].replace(',','.'))
             lon=float(row[7].replace(',','.'))
+            #On met dans la variable texte les informations que l'on veut lorsque l'on clique sur chaque Marker un a un
             texte=row[0]+': '+row[1]
-            folium.Marker(location=(lat,lon), popup=texte).add_to(fg)
+            #Création du marker avec toute les informations précédentes
+            folium.Marker(location=(lat,lon), popup=texte,icon=folium.Icon(icon="green")).add_to(fg)
 
 m.save("Carte_Interactive.html")
